521a3a0c72d7a91710f72c841efd42332547fbdf
Tobias De La Cruz León <tobias.delacruzleon@gmail.com>
2024-10-19 21:10:09 +0200

    ignoriere sync-to-pi.sh und darwin und linux bedienung für das install script

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Überprüfe das Betriebssystem
+os_type=$(uname)
+
 # Benutzereingaben abfragen
 read -p "Gib die IP-Adresse des Raspberry Pi ein: " ip_address
 read -p "Gib den Benutzernamen für den Raspberry Pi ein: " username
@@ -7,9 +10,22 @@ read -p "Gib den Ordnerpfad auf dem Raspberry Pi ein: " directory
 
 # Kopiere die Template-Datei und ersetze die Platzhalter
 cp sync-to-pi_template.sh sync-to-pi.sh
-sed -i "s/{{IP_ADDRESS}}/$ip_address/g" sync-to-pi.sh
-sed -i "s/{{USER}}/$username/g" sync-to-pi.sh
-sed -i "s|{{DIRECTORY}}|$directory|g" sync-to-pi.sh
+
+# Abhängig vom Betriebssystem den passenden sed-Befehl verwenden
+if [[ "$os_type" == "Darwin" ]]; then
+    # Für macOS
+    sed -i '' "s/{{IP_ADDRESS}}/$ip_address/g" sync-to-pi.sh
+    sed -i '' "s/{{USER}}/$username/g" sync-to-pi.sh
+    sed -i '' "s|{{DIRECTORY}}|$directory|g" sync-to-pi.sh
+elif [[ "$os_type" == "Linux" ]]; then
+    # Für Linux
+    sed -i "s/{{IP_ADDRESS}}/$ip_address/g" sync-to-pi.sh
+    sed -i "s/{{USER}}/$username/g" sync-to-pi.sh
+    sed -i "s|{{DIRECTORY}}|$directory|g" sync-to-pi.sh
+else
+    echo "Unbekanntes Betriebssystem: $os_type. Das Script unterstützt nur macOS und Linux."
+    exit 1
+fi
 
 # Setze die Rechte der neuen Datei auf ausführbar
 chmod +x sync-to-pi.sh
