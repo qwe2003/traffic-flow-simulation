be44c5da3cfb5728ef4297e6a2e2e0b383908a1c
Tobias De La Cruz León <tobias.delacruzleon@gmail.com>
2024-10-20 12:53:06 +0200

    move sync-to-pi_template.sh to scripts folder

--- scripts/sync-to-pi_template.sh
+++ scripts/sync-to-pi_template.sh
@@ -5,6 +5,8 @@ rsync -avz --delete --exclude 'node_modules' ./ {{USER}}@{{IP_ADDRESS}}:{{DIRECT
 # SSH-Befehl, um den Docker-Container auf dem Raspberry Pi neu zu starten
 ssh {{USER}}@{{IP_ADDRESS}} << 'EOF'
 cd {{DIRECTORY}}
-docker-compose down
-docker-compose up -d --build
+docker stop traffic-flow-simulator || true
+docker rm traffic-flow-simulator || true
+docker build -t traffic-flow-simulator .
+docker run -d -p 3000:3000 --name traffic-flow-simulator traffic-flow-simulator
 EOF
